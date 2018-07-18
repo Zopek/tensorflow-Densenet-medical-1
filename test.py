2797e508a0c8652f950cbb014449d2b7fe8cbb51
unknown <lethe-@sjtu.edu.cn>
2018-07-18 09:47:33 +0800

    查看样本分布

--- test.py
+++ test.py
@@ -2,15 +2,20 @@
 
 # import tensorflow as tf
 
-import input_data
+import input_data_for_patient as input_data
+import numpy as np
 
-filepath = '/DATA/data/qyzheng/lesion_one_normal_4'
-train_size, test_size = input_data.get_num(filepath)
-print(train_size)
+filepath = '/DATA/data/hyguan/liuyuan_spine/data_all/patient_image_4'
+train_size, test_size = input_data.get_size(filepath)
+train_dirs = input_data.get_train_dir(filepath)
+test_dirs = input_data.get_test_dir(filepath)
 
-batch_size = 100
-for step in range(3):
-	images, labels = input_data.train_next_batch(filepath, step, batch_size)
+for filename in train_dirs:
 
-	print(images.shape)
-	print('OK')
+	label = np.load(filepath + '/' + filename + '/label.npy')
+	labels = np.array([[0, 0, 0, 0], [0, 0, 0], [0, 0, 0, 0], [0, 0, 0]])
+	for i in range(4):
+		for j in range(4):
+			labels[i][label[j]] += 1
+
+print(labels)
